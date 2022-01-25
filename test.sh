a3f4dd5ae01f02d47a6fe54051d832996c5300e8
Om Kumar <97239340+0m4@users.noreply.github.com>
2022-01-26 01:18:55 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -6,21 +6,18 @@ echo -ne "
 
 Installing"
 sudo pacman -S --noconfirm xorg nvidia nvidia-utils plasma konsole dolphin ark kwrite kcalc spectacle krunner partitionmanager packagekit-qt5 sddm
-
 cd ~
 git clone "https://aur.archlinux.org/yay.git"
 cd ~/yay
 makepkg -si --noconfirm
 cd ~
-yay -S --noconfirm --needed zsh zsh-theme-powerlevel10k zsh-autosuggestions zsh-syntax-highlighting brave-bin spotify pamac-all timeshift
-
+yay -S zsh zsh-theme-powerlevel10k zsh-autosuggestions zsh-syntax-highlighting brave-bin spotify pamac-all timeshift
 sudo pacman -S --noconfirm virt-manager qemu ovmf vde2 ebtables dnsmasq bridge-utils openbsd-netcat qemu-arch-extra git openssh qbittorrent wget neofetch
 sudo systemctl enable sddm
 sudo systemctl start libvirtd.service
 sudo systemctl enable libvirtd.service
 sudo virsh net-start default 
 sudo virsh net-autostart default
-
 git clone https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
