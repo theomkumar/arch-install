0d89784430ab1254aece193405dd43c2161ff516
Om Kumar <97239340+0m4@users.noreply.github.com>
2022-01-26 04:19:45 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-if [ "`id -u`" -ne 0 ]
-then
-  echo -e "\n\nRun this script as root!\n\n"
-  exit -1
-fi
 echo -ne "
 -------------------------------------------------------------------------
     Installing Xorg,Plasma,nvidia,SDDM,YAY,ZSH-pk10,KVM
@@ -12,7 +7,7 @@ echo -ne "
 Installing
 "
 sudo pacman -S --noconfirm --needed xorg plasma konsole dolphin ark kwrite kcalc spectacle krunner partitionmanager packagekit-qt5
-
+sudo pacman -S --noconfirm --needed sddm virt-manager qemu ovmf vde2 ebtables dnsmasq bridge-utils openbsd-netcat qemu-arch-extra git openssh qbittorrent wget neofetch 
 cd ~
 git clone "https://aur.archlinux.org/yay.git"
 cd ~/yay
@@ -20,7 +15,6 @@ makepkg -si --noconfirm
 cd ~
 yay -S zsh zsh-theme-powerlevel10k zsh-autosuggestions zsh-syntax-highlighting timeshift --noconfirm --needed
 
-yes | pacman -S sddm virt-manager qemu ovmf vde2 ebtables dnsmasq bridge-utils openbsd-netcat qemu-arch-extra git openssh qbittorrent wget neofetch --needed
 systemctl enable sddm
 systemctl start libvirtd.service
 systemctl enable libvirtd.service
@@ -32,5 +26,5 @@ echo 'source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
 
-yay -S brave-bin spotify --needed --noconfirm
+yay -S brave-bin spotify 
 exit
