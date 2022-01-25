2ac8ed76ed7a0d742106c83843dc48f4aca62bce
Om Kumar <97239340+0m4@users.noreply.github.com>
2022-01-25 23:42:39 +0530

    Add files via upload

--- test.sh
+++ test.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 echo -ne "
 -------------------------------------------------------------------------
-                    My Packages
+    Installing Xorg,Plasma,nvidia,SDDM,YAY,ZSH-pk10,KVM
 -------------------------------------------------------------------------
 
 Installing"
-pacman -S xorg nvidia nvidia-utils plasma konsole dolphin ark kwrite kcalc spectacle krunner partitionmanager packagekit-qt5 sddm
-
-systemctl enable sddm
+sudo pacman -S xorg nvidia nvidia-utils plasma konsole dolphin ark kwrite kcalc spectacle krunner partitionmanager packagekit-qt5 sddm
 
 git clone https://aur.archlinux.org/yay.git
 cd yay
@@ -15,10 +13,15 @@ makepkg -si
 
 yay -S zsh zsh-theme-powerlevel10k zsh-autosuggestions zsh-syntax-highlighting brave-bin spotify pamac-all timeshift --noconfirm
 
-pacman -S virt-manager qemu ovmf vde2 ebtables dnsmasq bridge-utils openbsd-netcat qemu-arch-extra git openssh qbittorrent wget neofetch
+sudo pacman -S virt-manager qemu ovmf vde2 ebtables dnsmasq bridge-utils openbsd-netcat qemu-arch-extra git openssh qbittorrent wget neofetch
+sudo systemctl enable sddm
+sudo systemctl start libvirtd.service
+sudo systemctl enable libvirtd.service
+sudo virsh net-start default 
+sudo virsh net-autostart default
 
 git clone https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-echo 'source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh 
-source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh 
+echo 'source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
 exit
