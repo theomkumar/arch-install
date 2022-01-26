db765b8f784c2b683aa1d333e5b4876ac0300518
Om Kumar <97239340+0m4@users.noreply.github.com>
2022-01-26 15:48:25 +0530

    Update test2.sh

--- test2.sh
+++ test2.sh
@@ -6,8 +6,8 @@ echo -ne "
 #to speed up testing removed nvidia nvidia-utils 
 Installing
 "
-sudo pacman -S --noconfirm --needed xorg plasma konsole dolphin ark kwrite kcalc spectacle krunner partitionmanager packagekit-qt5
-sudo pacman -Rd --nodeps iptables --noconfirm
+sudo pacman -Sy --noconfirm --needed xorg plasma konsole dolphin ark kwrite kcalc spectacle krunner partitionmanager packagekit-qt5
+sudo pacman -Rdd iptables --noconfirm
 sudo pacman -S --noconfirm --needed sddm virt-manager qemu ovmf vde2 ebtables dnsmasq bridge-utils openbsd-netcat qemu-arch-extra git openssh qbittorrent wget neofetch
 
 sudo systemctl enable sddm
@@ -22,12 +22,12 @@ cd ~/yay
 makepkg -si --noconfirm
 cd ~
 yay -S zsh zsh-theme-powerlevel10k zsh-autosuggestions zsh-syntax-highlighting timeshift --noconfirm --needed
-
-yay -S brave-bin spotify --noconfirm
+yay -S snapd snapd-glib
+yay -Sdd brave-bin spotify --noconfirm
 
 git clone https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
-yay -S pamac-all
+yay -Sdd libpamac-full pamac-all --noconfirm
 exit
