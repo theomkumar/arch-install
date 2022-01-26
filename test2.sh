9300882261676fdcdd1cd843aede4383f39ba9b1
Om Kumar <97239340+0m4@users.noreply.github.com>
2022-01-26 20:13:30 +0530

    added time, locale , host, multilib root passwd

--- test2.sh
+++ test2.sh
@@ -1,15 +1,56 @@
 #!/bin/bash
 echo -ne "
 -------------------------------------------------------------------------
-    Installing Xorg,Plasma,nvidia,SDDM,YAY,ZSH-pk10,KVM
+    Installing GRUB,Xorg,Plasma,nvidia,SDDM,YAY,ZSH-pk10,KVM
 -------------------------------------------------------------------------
 #to speed up testing removed nvidia nvidia-utils 
 Installing
 "
-pacman -Sy reflector --noconfirm
+#TIME AND LOCALE
+
+ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
+hwclock --systohc
+nano /etc/locale.gen
+locale-gen
+echo LANG=en_US.UTF-8 >> /etc/locale.conf
+
+## HOST
+echo arch >> /etc/hostname
+nano 
+echo'
+
+127.0.0.1       localhost
+::1             localhost
+127.0.1.1       arch.localdomain        arch' >> /etc/hosts
+
+#Root password
+passwd
+#Mirrorlist
+sudo pacman -Sy 
 sudo systemctl enable reflector.timer
+sudo systemctl start sshd
+sudo systemctl enable sshd
 sudo systemctl start reflector.timer
 reflector --country India,Singapore,Indonesia --age 12 --sort rate --save /etc/pacman.d/mirrorlist
+sudo pacman -Syy base-devel linux-headers amd-ucode git nano vim reflector reflector openssh --noconfirm mtools dosfstools btrfs-progs --needed
+
+#GRUB NETWORK SOUND BLUETOOTH PRINT
+sudo pacman -S grub efibootmgr os-prober networkmanager network-manager-applet dialog xdg-utils xdg-user-dirs pipewire alsa-utils bluez bluez-utils cups --noconfirm
+sudo systemctl enable NetworkManager 
+sudo systemctl enable fstrim.timer
+sudo systemctl enable bluetooth.service
+sudo systemctl enable --now cups.service
+
+sudo grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB --noconfirm
+#Enable multilib
+sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
+
+sudo pacman -Sy --noconfirm
+
+#enable os prober
+echo 'GRUB_DISABLE_OS_PROBER=false' >> /etc/default/grub
+sudo grub-mkconfig -o /boot/grub/grub.cfg
+
 sudo pacman -Sy --noconfirm --needed xorg plasma konsole dolphin ark kwrite kcalc spectacle krunner partitionmanager packagekit-qt5
 sudo pacman -Rdd --noconfirm iptables 
 sudo pacman -S --noconfirm --needed sddm virt-manager qemu ovmf vde2 ebtables dnsmasq bridge-utils openbsd-netcat qemu-arch-extra git openssh qbittorrent wget neofetch
